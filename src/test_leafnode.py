28192ae9600d11df392d200d2956379cb8cfca72
green-waaagh <green.waaagh@gmail.com>
2026-03-07 20:54:00 +0300

    add leaf node

---
+++ src/test_leafnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Title")
+        self.assertEqual(node.to_html(), "<h1>Title</h1>")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_raw_text(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leaf_value_error(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
